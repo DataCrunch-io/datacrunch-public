603112950cf28ec1210c72570993a14c5f40cf78
tamirse <tamir@datacrunch.io>
2023-02-06 11:46:44 +0200

    Modified 18 files

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://stoplight.io/api/v1/projects/cHJqOjIzNTIy/spectral.js?branch_slug=main&token=665b9771-7532-46a3-a1e1-9ddce4bc4e79";
+export default { extends: ruleset };
